2af8509c024c85b3ec0400bc96e2f50cb127226e
Guilherme Oliveira <116916546+Guilherme841@users.noreply.github.com>
2022-12-27 12:06:33 -0300

    update

--- Curso Lógica de programação/ChutarNúmero.js
+++ Curso Lógica de programação/ChutarNúmero.js
@@ -9,15 +9,20 @@
 function aleatorio (min = 0, max = 10) {
     let numero = Math.random() * (max - min) + min
     random = numero.toFixed(0)
+    resultado.innerHTML = 'Chute um número!'
 }
 
 function chutar() {
     let resultado = document.getElementById('resultado')
     let chute = document.getElementById('chute')
     let shot = Number(chute.value)
-    if (shot == random) {
-        resultado.innerHTML = `Chute correto!`
+    if (shot < random) {
+        resultado.innerHTML = `Seu número é menor, tente outro!`
+    } else if (shot > random) {
+        resultado.innerHTML = `Seu número é maior, tente outro!`
     } else {
-        resultado.innerHTML = `Chute errado!`
+        var confirm = window.confirm('Chute correto! Jogar novamente?')
+    } if (confirm == true) {
+        document.body.onload(aleatorio())
     }
 }
