dc318181cb156b36dbc1596fed3dcbf1f472180b
Guilherme Oliveira <116916546+Guilherme841@users.noreply.github.com>
2023-03-12 18:22:41 -0300

    update

--- Curso coleções/Atividade.set.js
+++ Curso coleções/Atividade.set.js
@@ -2,7 +2,6 @@ const myArray = [30, 30, 40, 5, 223, 2049, 3034, 5];
 
 function valoresUnicos (arr) {
     const mySet = new Set(arr)
-
     return [...mySet];
 }
 
--- Curso de assíncrono/Pratica/script.js
+++ Curso de assíncrono/Pratica/script.js
@@ -1,22 +1,24 @@
-const BASE_URL = 'https://reqres.in/api/users?page=2'
-const gato = window.document.getElementById('cat')
-const change_cat = window.document.getElementById('change-cat')
+const BASE_URL = "https://reqres.in/api/users?page=2";
+const gato = window.document.getElementById("cat");
+const change_cat = window.document.getElementById("change-cat");
 
 const getCats = async () => {
-    try { 
-        const data = await fetch(BASE_URL);
-        const json = await data.json();
-
-        return json.avatar; 
-    } catch (error) {
-        console.log(error.message);
-    }
-}
+  try {
+    const data = await fetch(BASE_URL);
+    const json = await data.json();
+    const avatares = json.data.map(item => item.avatar);
+    console.log(avatares)
+    return avatares;
+  } catch (error) {
+    console.log(error.message);
+    throw new Error("Não foi possível carregar a imagem do gato :(");
+  }
+};
 
 const loadImg = async () => {
-    gato.src = await getCats();
-}
+  gato.src = await getCats();
+};
 
-change_cat.addEventListener('click', loadImg);
+change_cat.addEventListener("click", loadImg);
 
 loadImg();
