9d1e9f95db5673e72a5b15f96266d0423c3c2090
Guilherme Oliveira <128637003+Calendario-Da-Saude@users.noreply.github.com>
2023-04-15 21:07:12 -0300

    update

--- Curso de NodeJS/modules/express.js
+++ Curso de NodeJS/modules/express.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const UserModel = require('../src/models/user.model')
+const express = require("express");
+const UserModel = require("../src/models/user.model");
 
-const app = express()
+const app = express();
 app.use(express.json());
-app.set('view engine', 'ejs')
-app.set('views', "src/views");
+app.set("view engine", "ejs");
+app.set("views", "src/views");
 
 // app.get('/home', (req, res) => {
 //     res.contentType('application/html')
@@ -13,38 +13,38 @@ app.set('views', "src/views");
 
 // middlewares (função executada antes do express/ requisição)
 app.use((req, res, next) => {
-    console.log(`Request Type: ${req.method}`)
-    console.log(`Content Type: ${req.header["content-type"]}`)
-    console.log(`Date: ${new Date()}`)
-    next();
-})
-
-app.get('/views/users', async (req,res) => {
-    const users = await UserModel.find({});
-    res.render("index", {users});
-})
-
-app.get('/users', async (req, res) => {
-    try {
-        const users = await UserModel.find({});
-        res.status(200).json(users);
-    } catch (error) {
-        return res.status(500).send(error.message);
-    }
+  console.log(`Request Type: ${req.method}`);
+  console.log(`Content Type: ${req.header["content-type"]}`);
+  console.log(`Date: ${new Date()}`);
+  next();
 });
 
-app.get('/users/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const user = await UserModel.findById(id);
-        return res.status(200).json(user);
-    } catch (error) {
-        return res.status(500).send(error.message)
-    };
+app.get("/views/users", async (req, res) => {
+  const users = await UserModel.find({});
+  res.render("index", { users });
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await UserModel.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+app.get("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findById(id);
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
 });
 
 // app.patch('/users/:id', async (res, req) => {
-//     try { 
+//     try {
 //         const id = req.params.id;
 //         const user = await UserModel.findById(id, req.body,{new: true});
 //         return res.status(200).json(user);
@@ -53,36 +53,36 @@ app.get('/users/:id', async (req, res) => {
 //     }
 // });
 
-app.post('/users', async (req,res) => {
-    try {
-        const user = await UserModel.create(req.body) 
+app.post("/users", async (req, res) => {
+  try {
+    const user = await UserModel.create(req.body);
 
-    res.status(201).json(user); 
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
-app.patch('/users/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const user = await UserModel.findByIdAndUpdate(id, req.body, {new: true})
+app.patch("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
 
-        res.status(201).json(user);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-})
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 
-app.delete('/users/:id', async (req,res) => {
-    try {
-        const id = req.params.id;
-        const user = await UserModel.findByIdAndDelete(id)
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-})
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findByIdAndDelete(id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 
 const port = 5500;
 
