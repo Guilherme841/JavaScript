96d2e3a5b52d63296018cf66562f5ad0e3596e45
Guilherme Oliveira <Guilherme.ismael777@gmail.com>
2023-12-09 01:01:19 -0300

    update

--- Angular/primeiro-projeto-angular/src/app/app.component.ts
+++ Angular/primeiro-projeto-angular/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { PrimeiroComponent } from './componets/primeiro-component';
+import { MeuSegundoComponent } from './meu-segundo/meu-segundo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, PrimeiroComponent,MeuSegundoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
